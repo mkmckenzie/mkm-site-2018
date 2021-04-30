905f05d43b6b9a79348ff848cb912b1c1d9a348f
MK McKenzie <mary_katherine.mckenzie@stitchfix.com>
2021-04-30 12:44:22 -0700

    update work history to reflect promotion

--- src/content/WorkHistory.js
+++ src/content/WorkHistory.js
@@ -3,15 +3,37 @@ import React from 'react'
 
 const WorkHistoryContent = [
 														{
+															title: 'Lead Engineer 1',
+															company: 'Stitch Fix',
+															dates: 'May 2021 - Present',
+															description: <ul>
+																<li>Stack: Rails, React, PostgreSQL, GraphQL, TypeScript, Coming Soon: Redux</li>
+																<li>Projects: TBD!</li>
+																<li>Daily: TBD! Likely a continuation of daily duties from previous role, with an emphasis on mentorship and gaining technical expertise. </li>
+															</ul>,
+															order: 1,
+														},
+														{
 															title: 'Software Engineer 3',
 															company: 'Stitch Fix',
-															dates: 'June 2018 - Present',
+															dates: 'October 2019 - May 2021',
+															description: <ul>
+																<li>Stack: Rails, React, PostgreSQL, GraphQL, TypeScript</li>
+																<li>Projects: Shopping Bag features such as - Similar Items & Quantity Editing. Identified, architected, and added reusable components to the component library that were immediately leveraged by several teams.</li>
+																<li>Daily: Lead clearly defined projects related to Stitch Fix Shop features (including features within Shopping Bag, Product Detail Pages, and Shop index pages). Given product statement & design mocks - researched projects & broke up work into discrete milestones and clearly-defined JIRA tickets.  Pair with & mentor other engineers on my team to move roadmap work along. Continue to hone technical skills in React, TypeScript, and GraphQL. </li>
+															</ul>,
+															order: 2,
+														},
+														{
+															title: 'Software Engineer 2',
+															company: 'Stitch Fix',
+															dates: 'June 2018 - October 2019',
 															description: <ul>
 																<li>Stack: Rails, React, PostgreSQL</li>
 																<li>Projects: Consolidated Payment Form Experience, Structured Fix Request Redesign, Fix Inspiration Board, Direct Buy (Shop) features</li>
 																<li>Daily: Work cross-functionally to develop features for the user ("client") facing website, specifically regarding building client-stylist relationships for returning clients</li>
 															</ul>,
-															order: 1,
+															order: 3,
 														},
 														{
 															title: 'Graphics & Web Coordinator',
@@ -20,7 +42,7 @@ const WorkHistoryContent = [
 															description: <ul><li>Designed branding & marketing graphics for two major annual conferences</li>
 															<li>Created and updated content for the association website</li>
 															<li>Launched, marketed, and managed the online community for 8,000 members</li></ul>,
-															order: 3,
+															order: 5,
 														},
 														{
 															title: 'Software Engineer (Specialist)',
@@ -29,7 +51,7 @@ const WorkHistoryContent = [
 															description: <ul><li>Stack: Rails, React, Redux, MySQL, Elasticsearch</li>
 															<li>Projects: Interactive Venue Pricing, WeddingWire Pay integration with the Stripe API, Responsive Storefront & Write A Review Redesign</li>
 															<li>Daily: Implement a team-designed Agile workflow to work with product managers, designers, developers and quality assurance to see a feature from inception to completion, and beyond.</li></ul>,
-															order: 2,
+															order: 4,
 														}
 													];
 
