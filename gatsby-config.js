9e2034ceb0442a20cf6c4108aad8287b8566473d
MK McKenzie <--list>
2022-05-25 16:42:58 -0700

    more lint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Mary-Katherine McKenzie',
-  },
-  plugins: [
-  	'gatsby-plugin-react-helmet',
-  	{
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
+	siteMetadata: {
+		title: 'Mary-Katherine McKenzie',
+	},
+	plugins: [
+    'gatsby-plugin-react-helmet',
     {
-    resolve: `gatsby-plugin-google-analytics`,
-    options: {
-      trackingId: "UA-41941177-1",
-      // Puts tracking script in the head instead of the body
-      head: true,
-      // Setting this parameter is optional
-      anonymize: false,
-      // Setting this parameter is also optional
-      respectDNT: true,
-    },
-  },
-  ],
+			resolve: 'gatsby-plugin-typography',
+			options: {
+				pathToConfigModule: 'src/utils/typography',
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-google-analytics',
+			options: {
+				trackingId: 'UA-41941177-1',
+				// Puts tracking script in the head instead of the body
+				head: true,
+				// Setting this parameter is optional
+				anonymize: false,
+				// Setting this parameter is also optional
+				respectDNT: true,
+			},
+		},
+	],
 };
